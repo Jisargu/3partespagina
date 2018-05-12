de12bab06b3041cc222fa18fd2da76e4024fb4f4
Jisargu <jonathnsaa@gmail.com>
2018-05-11 22:54:31 -0500

    Creacion de newsletter.  Se creo una variable newsletter para el contenido de la sección NEWSLETTER

--- pag/src/app/footer/footer.component.ts
+++ pag/src/app/footer/footer.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
+  newsletter = "Sit vel delectus amet officiis repudiandae est voluptatem. Tempora maxime provident nisi et fuga et enim exercitationem ipsam. Culpa consequatur occaecati.";
   constructor() { }
 
   ngOnInit() {
