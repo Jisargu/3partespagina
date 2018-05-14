cbb0293943eb468e5ff000e249a3b74edf0fc5aa
Jisargu <jonathnsaa@gmail.com>
2018-05-14 01:21:42 -0500

    3 partes funcionales de modulos angular

--- pag/src/app/app.module.ts
+++ pag/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PopularPostsComponent } from './popular-posts/popular-posts.component';
-import { AppComponent } from './app.component'; 
+
 import { ImagenesPoderosasComponent } from './imagenes-poderosas/imagenes-poderosas.component';
 
 import { FooterComponent } from './footer/footer.component';
